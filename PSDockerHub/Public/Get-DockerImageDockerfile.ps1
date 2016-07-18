5c70bd2702df1b86dc224204ecc4683841f680d8
beatcracker <beatcracker@users.noreply.github.com>
2016-07-18 03:21:43 +0300

    Get-DockerImageDockerfile

---
+++ PSDockerHub/Public/Get-DockerImageDockerfile.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+    Get Docker image Dockerfile.
+
+.Description
+    Get Docker image Dockerfile.    
+    Build history is available only for some automated builds.
+
+.Parameter Name
+    Docker repository (image) name
+
+.Example
+    Get-DockerImageDockerfile -Name 'jwilder/nginx-proxy'
+
+    Get Dockerfile for 'jwilder/nginx-proxy' image
+
+#>
+function Get-DockerImageDockerfile
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$Name
+    )
+
+    Begin
+    {
+        $RequestTpl = 'repositories/{0}/dockerfile/'
+    }
+
+    Process
+    {
+        foreach ($item in $Name) {
+            $Request = $RequestTpl -f ($item | Resolve-DockerHubRepoName)
+            (Invoke-DockerHubWebRequest -Request $Request).contents
+        }
+    }
+}
