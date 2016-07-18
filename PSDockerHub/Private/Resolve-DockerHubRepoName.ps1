4914e788bac2170eadea334ea40ce5bfeeb3e69b
beatcracker <beatcracker@users.noreply.github.com>
2016-07-18 03:20:09 +0300

    Function to resolve 'official' repo names

---
+++ PSDockerHub/Private/Resolve-DockerHubRepoName.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.Synopsis
+    Adds 'library' path component to the piped name, if it has only one path component.
+    Used to resolve names for official repositories like 'alpine' and 'mariadb'.
+
+.Link
+    https://docs.docker.com/registry/spec/api/
+#>
+filter Resolve-DockerHubRepoName {
+    if ($_ -match '/') {
+        $_
+    } else {
+        "library/$_"
+    }
+}
