8e036e88dc65a27d7c283b68a3ab2004087c65df
beatcracker <beatcracker@users.noreply.github.com>
2016-07-18 03:20:42 +0300

    Find-DockerImage

---
+++ PSDockerHub/Public/Find-DockerImage.ps1
@@ -0,0 +1,126 @@
+﻿<#
+.Synopsis
+    Search for docker images on Docker Hub
+
+.Description
+    Search for docker images on Docker Hub via Docker Hub API.
+    You can filter search by Name/Description, Stars, Downloads, Official images and Automated builds.
+
+.Parameter SearchTerm
+    Search term. Docker Hub will search it in the Name and Description.
+    If search term is not specified, function will return all available images using wildcard query ('*').
+
+.Parameter SortBy
+    Sort by Downloads or Stars. Sorting is performed on Docker Hub side.
+
+.Parameter Automated
+    Search only for automated builds
+    Note, that this parameter is ignored, when SearchTerm is omitted (wildcard query)
+
+.Parameter Official
+    Search only for official images
+    Note, that this parameter is ignored, when SearchTerm is omitted (wildcard query)
+
+.Parameter MaxResults
+    Maximum number of results to return. Default is 25.
+
+.Example
+    Find-DockerImage -SearchTerm mariadb -Official
+
+    Search for official MariaDB docker images
+
+.Example
+    Find-DockerImage -SearchTerm ruby -SortBy Stars -Automated -MaxResults 5
+
+    Search for automated builds of 'ruby', sort by stars, return 5 top results
+
+.Example
+    Find-DockerImage -SearchTerm mariadb -SortBy Downloads | Where-Object {$_.Name -like '*alpine*'}
+
+    Search for MariaDB docker images, sort by downloads. Then find images built on Alpine Linux using PowerShell filtering.
+
+.Example
+    Find-DockerImage -SortBy Downloads
+
+    Get most downloaded docker images
+
+
+.Example
+    Find-DockerImage -SortBy Stars
+
+    Get most starred docker images
+#>
+function Find-DockerImage
+{
+    [CmdletBinding(DefaultParameterSetName = 'Base')]
+    Param
+    (
+        [Parameter(ValueFromPipeline = $true, ParameterSetName = 'Base')]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$SearchTerm = '*',
+
+        [Parameter(ParameterSetName = 'Base')]
+        [Parameter(ParameterSetName = 'SortBy')]
+        [ValidateSet('Downloads', 'Stars')]
+        [string]$SortBy,
+
+        [Parameter(ParameterSetName = 'Base')]
+        [Parameter(ParameterSetName = 'Automated')]
+        [switch]$Automated,
+
+        [Parameter(ParameterSetName = 'Base')]
+        [Parameter(ParameterSetName = 'Official')]
+        [switch]$Official,
+
+        [Parameter(ParameterSetName = 'Base')]
+        [Parameter(ParameterSetName = 'SortBy')]
+        [Parameter(ParameterSetName = 'Automated')]
+        [Parameter(ParameterSetName = 'Official')]
+        [int]$MaxResults = 25
+
+    )
+
+    Begin
+    {
+        $RequestTpl = 'search/repositories/?query={0}'
+        $QueryParamMap = @{
+            Automated = 'is_automated=1'
+            Official = 'is_official=1'
+            Downloads = 'ordering=-pull_count'
+            Stars = 'ordering=-star_count'
+        }
+    }
+
+    Process
+    {
+        $QueryParams = @()
+
+        if ($Automated) {
+            $QueryParams += $QueryParamMap.Automated
+        }
+
+        if ($Official) {
+            $QueryParams += $QueryParamMap.Official
+        }
+
+        if ($SortBy) {
+            $QueryParams += $QueryParamMap.$SortBy
+        }
+
+        $Request = $SearchTerm | ForEach-Object {
+            (
+                @($RequestTpl -f [System.Net.WebUtility]::UrlEncode($_)) + $QueryParams
+            ) -join '&'
+        }
+
+        Invoke-DockerHubWebRequest -Request $Request -Paginated -MaxResults $MaxResults |
+            Select-Object -Property (
+                @{n = 'Name' ; e = {$_.repo_name}},
+                @{n = 'Description' ; e = {$_.short_description}},
+                @{n = 'Stars' ; e = {$_.star_count}},
+                @{n = 'Downloads' ; e = {$_.pull_count}},
+                @{n = 'Official' ; e = {$_.is_official}},
+                @{n = 'Automated' ; e = {$_.is_automated}}
+            ) | Add-TypeName -TypeName $PSCmdlet.MyInvocation.MyCommand.Name
+    }
+}
