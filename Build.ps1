5aa712f4edbc917a461ddfa072aab08b32240d36
beatcracker <beatcracker@users.noreply.github.com>
2018-05-23 00:38:25 +0300

    Add "-SkipPublisherCheck" for Pester

--- Build.ps1
+++ Build.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop'
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module PSDeploy, BuildHelpers, Psake, Pester, PSScriptAnalyzer -Force
+Install-Module PSDeploy, BuildHelpers, Psake, Pester, PSScriptAnalyzer -Force -SkipPublisherCheck
 Import-Module Psake, BuildHelpers
 
 Set-BuildEnvironment
